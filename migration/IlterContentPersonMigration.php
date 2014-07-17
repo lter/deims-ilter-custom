2aaa28a982d4c2ee32678abdeb20894f2d2034eb
Inigo San Gil <isangil@lternet.edu>
2014-07-17 03:33:40 +0100

    Removes unneeded relations from person migration

--- migration/IlterContentPersonMigration.php
+++ migration/IlterContentPersonMigration.php
@@ -29,16 +29,18 @@ class IlterContentPersonMigration extends DeimsContentPersonMigration {
     $this->addFieldMapping('field_images:file_class')->defaultValue('MigrateFileFid');
     $this->addFieldMapping('field_images:preserve_files')->defaultValue(TRUE);
 
+    // Vocabularies (other nets)
+    $this->addFieldMapping('field_networks_term_ref','field_research_site_network')
+     ->sourceMigration('IlterTaxonomyNetwork');
+    $this->addFieldMapping('field_networks_term_ref:source_type')
+      ->defaultValue('tid');
+
     // Vocabularies (3 term refs)
     $this->addFieldMapping('field_person_specialty_term','field_person_specilaty')
       ->sourceMigration('IlterTaxonomyILTERDiscipline');
     $this->addFieldMapping('field_person_specialty_term:source_type')
       ->defaultValue('tid');
 
-    $this->addFieldMapping('field_networks_term_ref','field_person_network')
-     ->sourceMigration('IlterTaxonomyNetwork');
-    $this->addFieldMapping('field_networks_term_ref:source_type')
-      ->defaultValue('tid');
 
     // field_person_subjects	Subjects Investigated (term ref subjects envTheOLD )
     $this->addFieldMapping('field_subjects_term_ref','field_person_subjects')
@@ -51,12 +53,12 @@ class IlterContentPersonMigration extends DeimsContentPersonMigration {
     // Node ref to Ilter Natl. Network.
     $this->addFieldMapping('field_person_network_ref','field_person_network')
       ->sourceMigration('IlterContentILTERNationalNetwork');
-
-
+/**
+     First take: instead of creating Stubs, lets just ignore it for now.
     // Node Red to "site".
     $this->addFieldMapping('field_related_sites','field_dataset_site_name')
       ->sourceMigration('IlterContentSite');
-
+*/
     $this->addFieldMapping('field_address:postal_code','field_person_postalcode');
 
     // field_person_pubs does not exist
@@ -75,8 +77,8 @@ class IlterContentPersonMigration extends DeimsContentPersonMigration {
       'revision_uid',
       'field_person_pubs',
       'field_person_photo_data',
-      'field_dataset_site_ref', // it is a dupe: Site node ref 
       'field_person_rel_dataset', // from the inverse relation - Related datasets
+      'field_dataset_site_name',  // DNM
     ));
 
     $this->addUnmigratedDestinations(array(
