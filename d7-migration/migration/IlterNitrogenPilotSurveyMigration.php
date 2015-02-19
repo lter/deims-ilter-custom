0ed7d56e3b9d3ea0a445681638326f0b0adbea7f
isangil <isangil@lternet.edu>
2015-02-19 06:32:23 -0700

    A more complete custom field migration

--- d7-migration/migration/IlterNitrogenPilotSurveyMigration.php
+++ d7-migration/migration/IlterNitrogenPilotSurveyMigration.php
@@ -32,27 +32,55 @@ class IlterNitrogenPilotSurveyMigration extends DrupalNode7Migration {
    $this->addFieldMapping('field_upload_pdf','field_upload_pdf');	//Upload PDF
    $this->addFieldMapping('field_upload_pdf:display','field_upload_pdf:display'); //	Upload PDF subfield
 
-/**field_upload_pdf:description	Upload PDF subfield
-field_research_paper_available	Research Paper Available
-field_data_available	Data Available
-field__ilter_site_data	ILTER Site - data
-field_eml_available	EML Available (Click if yes)
-field_upload_eml	Upload EML
-field_data_doi_url	Data DOI or Download URL
-field_data_doi_url:title	Data DOI or Download URL subfield
-field_data_doi_url:attributes	Data DOI or Download URL subfield
-field_upload_data_paper_	Upload Data Paper
-field_upload_data_paper_:display	Upload Data Paper subfield
-field_upload_data_paper_:description	Upload Data Paper subfield
-field_ilter_person	ILTER Person
-field_join_synthesis_team_	Join Synthesis Team
-field_new_research_theme	Are you interested in implementing actual observation in your site?
-field_theme_paper	Theme of Paper
-field_data_theme
-*/
+   $this->addSimpleMappings(array(
+     'field_upload_pdf:description',  //	Upload PDF subfield
+     'field_research_paper_available',	// Research Paper Available
+     'field_data_available',  //	Data Available
+     'field__ilter_site_data',	// ILTER Site - data
+     'field_eml_available',	// EML Available (Click if yes)
+     'field_upload_eml',	// Upload EML
+     'field_data_doi_url',	// Data DOI or Download URL
+     'field_data_doi_url:title',	// Data DOI or Download URL subfield
+     'field_data_doi_url:attributes', 	// Data DOI or Download URL subfield
+     'field_upload_data_paper_',	// Upload Data Paper
+     'field_upload_data_paper_:display',	//Upload Data Paper subfield
+     'field_upload_data_paper_:description',	//Upload Data Paper subfield
+     'field_ilter_person',	// ILTER Person
+     'field_join_synthesis_team_',	// Join Synthesis Team
+     'field_new_research_theme',	//Are you interested in implementing actual observation in your site?
+     'field_theme_paper',	// Theme of Paper
+     'field_data_theme',
+    ));
 
    $this->addUnmigratedDestinations(array(
      'body',
+     'field_paper_doi:language',      // 	Subfield: Language for the field
+     'field_upload_data_paper_:file_class',      // 	Option: Implementation of MigrateFile to use
+     'field_upload_data_paper_:language',      // 	Subfield: Language for the field
+     'field_upload_data_paper_:preserve_files',      // 	Option: Boolean indicating whether files should be preserved or deleted on rollback
+     'field_upload_data_paper_:destination_dir',      // 	Subfield: Path within Drupal files directory to store file
+     'field_upload_data_paper_:destination_file',      // 	Subfield: Path within destination_dir to store the file.
+     'field_upload_data_paper_:file_replace',      // 	Option: Value of $replace in that file function. Defaults to FILE_EXISTS_RENAME.
+     'field_upload_data_paper_:source_dir',      // 	Subfield: Path to source file.
+     'field_upload_data_paper_:urlencode',      // 	Option: Encode all segments of the incoming path (defaults to TRUE).
+     'field_upload_eml:file_class',      // 	Option: Implementation of MigrateFile to use
+     'field_upload_eml:language',      // 	Subfield: Language for the field
+     'field_upload_eml:preserve_files',      // 	Option: Boolean indicating whether files should be preserved or deleted on rollback
+     'field_upload_eml:destination_dir',      // 	Subfield: Path within Drupal files directory to store file
+     'field_upload_eml:destination_file',      // 	Subfield: Path within destination_dir to store the file.
+     'field_upload_eml:file_replace',      // 	Option: Value of $replace in that file function. Defaults to FILE_EXISTS_RENAME.
+     'field_upload_eml:source_dir',      // 	Subfield: Path to source file.
+     'field_upload_eml:urlencode',      // 	Option: Encode all segments of the incoming path (defaults to TRUE).
+     'field_upload_eml:description',      // 	Subfield: String to be used as the description value
+     'field_upload_eml:display',      // 
+     'field_upload_pdf:file_class',      // 	Option: Implementation of MigrateFile to use
+     'field_upload_pdf:language',      // 	Subfield: Language for the field
+     'field_upload_pdf:preserve_files',      // 	Option: Boolean indicating whether files should be preserved or deleted on rollback
+     'field_upload_pdf:destination_dir',      // 	Subfield: Path within Drupal files directory to store file
+     'field_upload_pdf:destination_file',      // 	Subfield: Path within destination_dir to store the file.
+     'field_upload_pdf:file_replace',      // 	Option: Value of $replace in that file function. Defaults to FILE_EXISTS_RENAME.
+     'field_upload_pdf:source_dir',      // 	Subfield: Path to source file.
+     'field_upload_pdf:urlencode',	// Option: Encode all segments of the incoming path (defaults to TRUE).
    ));
   }
   public function prepareRow($row) {
