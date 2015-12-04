660041012484e4ad43f92a2eb1c46df82998b280
isangil <isangil@lternet.edu>
2015-12-04 16:25:19 -0700

    Corrects the filename migration

--- migration/IlterFileMigration.php
+++ migration/IlterFileMigration.php
@@ -9,25 +9,6 @@ class IlterFileMigration extends DeimsFileMigration {
 
   public function prepare($file, $row) {
 
-
-    // Add data for documents from the CCT "Resources"
-
-    $connection = Database::getConnection('default', $this->sourceConnection);
-    $query = $connection->select('node', 'n');
-    $query->condition('n.type', 'resources');
-    $query->join('node_revisions', 'nr', 'n.vid = nr.vid');
-    $query->fields('nr', array('title'));
-
-//   There is a "link" field to ext resources, not sure what would i do with it.
-//    $query->join('content_type_resources', 'ctd', 'n.vid = ctd.vid');
-//    $query->condition('ctd.field_download_file_fid', $row->fid);
-
-    if ($result = $query->execute()->fetch()) {
-      if (!empty($result->title)) {
-        $file->filename = $result->title;
-      } 
-    }
-
     // Hack to make migration work as we expect.
 
     $file->value = 'public://' . str_replace("sites/data.lter-europe.net.deims/files/","",$file->value);
@@ -35,6 +16,6 @@ class IlterFileMigration extends DeimsFileMigration {
     if (!file_exists($file->value)) {
       throw new MigrateException("The file at {$file->value} does not exist.");
     }
-//    parent::prepare($file, $row);
+
   }
 }
